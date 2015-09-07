8c5edcc8e18d432d704b58e04c5d229a3825e9fd
Matt Bellis <matthew.bellis@gmail.com>
2015-09-07 16:58:21 -0400

    Changed the first lecture.

--- lectures/LECTURE_printing_and_assignments.py
+++ lectures/LECTURE_printing_and_assignments.py
@@ -1,13 +1,29 @@
+# Comments
+
 # How do we print something to the screen?
 
-# How do we assign values?
 
-# What's a variable?
-
-# Can it change?
-
-# What's the difference?
-
-# Then what?
+print "-------------------"
+print "Assigning values"
 
 
+print "* ------"
+print "Types"
+
+print "--------------"
+print "Math stuff"
+
+print "* -------------"
+print "Incrementing values"
+
+print "------------------------"
+print "* Other stuff ---------"
+
+print "* -----------"
+print "Harder math"
+
+print "--------------------"
+print "Better print formatting"
+
+print "-----------------"
+print "* String formatting"
